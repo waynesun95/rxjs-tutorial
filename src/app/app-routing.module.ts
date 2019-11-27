9f4816238c4cdace067a463895ab966f7dc46ee4
Wayne Sun <waynesun95@gmail.com>
2019-11-26 22:55:31 -0500

    feat(create operator): create operator

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CreateComponent } from './observables/creating/create/create.component';
+import { CreateComponent } from './operators/creating/create/create.component';
 import { HomeComponent } from './home/home.component';
 
 
@@ -8,6 +8,15 @@ const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'operators',
+    children: [
+      {
+        path: 'create',
+        component: CreateComponent
+      }
+    ]
   }
 ];
 
